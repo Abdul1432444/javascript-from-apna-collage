032c2aba93e48be975590ec7d4bc9e9a5e3eaffb
Abdul1432444 <ar9834606@gmail.com>
2025-09-22 10:07:44 +0500

    done

--- rock scissors game/script.js
+++ rock scissors game/script.js
@@ -2,19 +2,29 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
 const genCompChoice = () => {
   const options = ["rock", "paper", "scissors"];
   const randIdx = Math.floor(Math.random() * 3);
   return options[randIdx];
 };
-const drawGame = () => {
-  console.log("Game draw shu");
+const drawGame = (userChoice, compChoice) => {
+  msg.innerText = `Game Draw! your ${userChoice} and computer choice ${compChoice} are same `;
+  msg.style.backgroundColor = "rgb(14, 14, 32)";
 };
-const showWinner = (userWin) => {
+const showWinner = (userWin, userChoice, compChoice) => {
   if (userWin) {
-    console.log("you Win");
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You Win! your ${userChoice} beats computer ${compChoice}`;
+    msg.style.backgroundColor = "green";
   } else {
-    console.log("you lose");
+    compScore++;
+    compScorePara.innerHTML = compScore;
+    msg.innerText = ` You Lose! computer ${compChoice} beats your ${userChoice} badly`;
+    msg.style.backgroundColor = "red";
   }
 };
 const playGame = (userChoice) => {
@@ -22,7 +32,7 @@ const playGame = (userChoice) => {
   const compChoice = genCompChoice();
   console.log("CompChoice=", compChoice);
   if (userChoice === compChoice) {
-    drawGame();
+    drawGame(userChoice, compChoice);
   } else {
     let userWin = true;
     if (userChoice === "rock") {
@@ -32,7 +42,7 @@ const playGame = (userChoice) => {
     } else if (userChoice === "scissors") {
       userWin = compChoice === "rock" ? false : true;
     }
-    showWinner(userWin);
+    showWinner(userWin, userChoice, compChoice);
   }
 };
 choices.forEach((choice) => {
