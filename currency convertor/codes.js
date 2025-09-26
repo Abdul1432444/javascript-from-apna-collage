0c7892e70d4f8e3a97f30cea31302a865bde748f
Abdul1432444 <ar9834606@gmail.com>
2025-09-26 10:39:07 +0500

    done

---
+++ currency convertor/codes.js
@@ -0,0 +1,140 @@
+const countryList = {
+  AED: "AE", // United Arab Emirates Dirham
+  AFN: "AF", // Afghanistan Afghani
+  ALL: "AL", // Albania Lek
+  AMD: "AM", // Armenia Dram
+  ANG: "AN", // Netherlands Antillean Guilder
+  AOA: "AO", // Angola Kwanza
+  ARS: "AR", // Argentina Peso
+  AUD: "AU", // Australia Dollar
+  AZN: "AZ", // Azerbaijan Manat
+  BAM: "BA", // Bosnia-Herzegovina Convertible Mark
+  BBD: "BB", // Barbados Dollar
+  BDT: "BD", // Bangladesh Taka
+  BGN: "BG", // Bulgaria Lev
+  BHD: "BH", // Bahrain Dinar
+  BIF: "BI", // Burundi Franc
+  BMD: "BM", // Bermuda Dollar
+  BND: "BN", // Brunei Dollar
+  BOB: "BO", // Bolivia Boliviano
+  BRL: "BR", // Brazil Real
+  BSD: "BS", // Bahamas Dollar
+  BTN: "BT", // Bhutan Ngultrum
+  BWP: "BW", // Botswana Pula
+  BYN: "BY", // Belarus Ruble
+  BZD: "BZ", // Belize Dollar
+  CAD: "CA", // Canada Dollar
+  CDF: "CD", // Congo Franc
+  CHF: "CH", // Switzerland Franc
+  CLP: "CL", // Chile Peso
+  CNY: "CN", // China Yuan
+  COP: "CO", // Colombia Peso
+  CRC: "CR", // Costa Rica Colon
+  CUP: "CU", // Cuba Peso
+  CZK: "CZ", // Czech Republic Koruna
+  DJF: "DJ", // Djibouti Franc
+  DKK: "DK", // Denmark Krone
+  DOP: "DO", // Dominican Republic Peso
+  DZD: "DZ", // Algeria Dinar
+  EGP: "EG", // Egypt Pound
+  ERN: "ER", // Eritrea Nakfa
+  ETB: "ET", // Ethiopia Birr
+  EUR: "EU", // Euro
+  FJD: "FJ", // Fiji Dollar
+  GBP: "GB", // United Kingdom Pound
+  GEL: "GE", // Georgia Lari
+  GHS: "GH", // Ghana Cedi
+  GMD: "GM", // Gambia Dalasi
+  GNF: "GN", // Guinea Franc
+  GTQ: "GT", // Guatemala Quetzal
+  GYD: "GY", // Guyana Dollar
+  HKD: "HK", // Hong Kong Dollar
+  HNL: "HN", // Honduras Lempira
+  HRK: "HR", // Croatia Kuna
+  HTG: "HT", // Haiti Gourde
+  HUF: "HU", // Hungary Forint
+  IDR: "ID", // Indonesia Rupiah
+  ILS: "IL", // Israel Shekel
+  INR: "IN", // India Rupee
+  IQD: "IQ", // Iraq Dinar
+  IRR: "IR", // Iran Rial
+  ISK: "IS", // Iceland Krona
+  JMD: "JM", // Jamaica Dollar
+  JOD: "JO", // Jordan Dinar
+  JPY: "JP", // Japan Yen
+  KES: "KE", // Kenya Shilling
+  KGS: "KG", // Kyrgyzstan Som
+  KHR: "KH", // Cambodia Riel
+  KMF: "KM", // Comoros Franc
+  KRW: "KR", // South Korea Won
+  KWD: "KW", // Kuwait Dinar
+  KZT: "KZ", // Kazakhstan Tenge
+  LAK: "LA", // Laos Kip
+  LBP: "LB", // Lebanon Pound
+  LKR: "LK", // Sri Lanka Rupee
+  LRD: "LR", // Liberia Dollar
+  LYD: "LY", // Libya Dinar
+  MAD: "MA", // Morocco Dirham
+  MDL: "MD", // Moldova Leu
+  MGA: "MG", // Madagascar Ariary
+  MKD: "MK", // North Macedonia Denar
+  MMK: "MM", // Myanmar Kyat
+  MNT: "MN", // Mongolia Tugrik
+  MOP: "MO", // Macau Pataca
+  MUR: "MU", // Mauritius Rupee
+  MVR: "MV", // Maldives Rufiyaa
+  MWK: "MW", // Malawi Kwacha
+  MXN: "MX", // Mexico Peso
+  MYR: "MY", // Malaysia Ringgit
+  MZN: "MZ", // Mozambique Metical
+  NAD: "NA", // Namibia Dollar
+  NGN: "NG", // Nigeria Naira
+  NIO: "NI", // Nicaragua Cordoba
+  NOK: "NO", // Norway Krone
+  NPR: "NP", // Nepal Rupee
+  NZD: "NZ", // New Zealand Dollar
+  OMR: "OM", // Oman Rial
+  PAB: "PA", // Panama Balboa
+  PEN: "PE", // Peru Sol
+  PGK: "PG", // Papua New Guinea Kina
+  PHP: "PH", // Philippines Peso
+  PKR: "PK", // Pakistan Rupee
+  PLN: "PL", // Poland Zloty
+  PYG: "PY", // Paraguay Guarani
+  QAR: "QA", // Qatar Riyal
+  RON: "RO", // Romania Leu
+  RSD: "RS", // Serbia Dinar
+  RUB: "RU", // Russia Ruble
+  RWF: "RW", // Rwanda Franc
+  SAR: "SA", // Saudi Arabia Riyal
+  SBD: "SB", // Solomon Islands Dollar
+  SCR: "SC", // Seychelles Rupee
+  SDG: "SD", // Sudan Pound
+  SEK: "SE", // Sweden Krona
+  SGD: "SG", // Singapore Dollar
+  SLL: "SL", // Sierra Leone Leone
+  SOS: "SO", // Somalia Shilling
+  SRD: "SR", // Suriname Dollar
+  SYP: "SY", // Syria Pound
+  THB: "TH", // Thailand Baht
+  TND: "TN", // Tunisia Dinar
+  TOP: "TO", // Tonga Paʻanga
+  TRY: "TR", // Turkey Lira
+  TTD: "TT", // Trinidad and Tobago Dollar
+  TWD: "TW", // Taiwan Dollar
+  TZS: "TZ", // Tanzania Shilling
+  UAH: "UA", // Ukraine Hryvnia
+  UGX: "UG", // Uganda Shilling
+  USD: "US", // United States Dollar
+  UYU: "UY", // Uruguay Peso
+  UZS: "UZ", // Uzbekistan Som
+  VEF: "VE", // Venezuela Bolivar
+  VND: "VN", // Vietnam Dong
+  XAF: "CM", // Central Africa CFA Franc
+  XCD: "AG", // East Caribbean Dollar
+  XOF: "SN", // West African CFA Franc
+  YER: "YE", // Yemen Rial
+  ZAR: "ZA", // South Africa Rand
+  ZMW: "ZM", // Zambia Kwacha
+  ZWL: "ZW", // Zimbabwe Dollar
+};
